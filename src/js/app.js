fb0dc6a523bcd358cf3f436df3c69d090e76dafe
Victoria <vicktorianedopekina@yandex.ru>
2022-04-11 23:30:21 +0400

    four block

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,3 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-const burger = document.querySelector(".header__toggle");
-
-document.querySelector('.header__menu-burger').addEventListener('click', function() {
-    document.querySelector('.header__nav').classList.toggle('active');
-    document.querySelector('body').classList.toggle('lock');
-    //document.querySelector('.header__toggle').classList.remove('burger-close');
-    //document.querySelector('.header__toggle').classList.toggle('burger-open');
-        if (burger.classList.contains('burger-close')) {
-            burger.classList.remove('burger-close');
-            burger.classList.toggle('burger-open');
-        } else if (burger.classList.contains('burger-open')) {
-            burger.classList.remove('burger-open');
-            burger.classList.toggle('burger-close');
-        }
-});
